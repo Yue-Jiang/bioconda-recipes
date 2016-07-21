d5ec0e11c3e6029394b363daa261bdf18501edb2
Bjoern Gruening <bjoern.gruening@gmail.com>
2016-07-21 23:18:46 +0200

    add openms

---
+++ recipes/openms/build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+wget https://github.com/OpenMS/contrib/archive/e8f628db990010ae67fe2c466544b5ce50a846da.tar.gz
+
+tar xfvz e8f628db990010ae67fe2c466544b5ce50a846da.tar.gz
+
+mv contrib-e8f628db990010ae67fe2c466544b5ce50a846da contrib
+cd contrib
+
+cmake . -DBUILD_TYPE=SEQAN
+cmake . -DBUILD_TYPE=LIBSVM
+cmake . -DBUILD_TYPE=XERCESC
+cmake . -DBUILD_TYPE=GSL
+cmake . -DBUILD_TYPE=BOOST -DNUMBER_OF_JOBS=4
+cmake . -DBUILD_TYPE=COINOR
+cmake . -DBUILD_TYPE=BZIP2
+cmake . -DBUILD_TYPE=GLPK
+cmake . -DBUILD_TYPE=EIGEN
+cmake . -DBUILD_TYPE=WILDMAGIC
+
+cd ..
+mkdir build
+cd build
+ORIGIN='$ORIGIN'
+export ORIGIN
+LDFLAGS='-Wl,-rpath,$${ORIGIN}/../lib' cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DHAS_XSERVER=OFF -DENABLE_TUTORIALS=OFF -DENABLE_STYLE_TESTING=OFF -DENABLE_UNITYBUILD=OFF -DWITH_GUI=OFF
+make OpenMS TOPP UTILS
+make install
