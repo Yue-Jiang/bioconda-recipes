9ec68e9fa1f0931fab06b0df857f155d0166b86b
Bjoern Gruening <bjoern.gruening@gmail.com>
2016-07-23 00:23:40 +0200

    add newer wget version

--- recipes/openms/build.sh
+++ recipes/openms/build.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
 
-curl -L https://api.github.com/repos/OpenMS/contrib/tarball/e8f628db990010ae67fe2c466544b5ce50a846da | tar zx
-
-#sed -i s/1aecb390311a12330c5a1c8d91cb9ac4cd8e1718/da39a3ee5e6b4b0d3255bfef95601890afd80709/ CMakeLists.txt
-
-ls -l
-
-mv OpenMS-contrib-e8f628d contrib
 cd contrib
 
-cmake . -DBUILD_TYPE=SEQAN
 cmake . -DBUILD_TYPE=LIBSVM
 cmake . -DBUILD_TYPE=XERCESC
 cmake . -DBUILD_TYPE=GSL
@@ -19,7 +11,7 @@ cmake . -DBUILD_TYPE=BZIP2
 cmake . -DBUILD_TYPE=GLPK
 cmake . -DBUILD_TYPE=EIGEN
 cmake . -DBUILD_TYPE=WILDMAGIC
-
+cmake . -DBUILD_TYPE=SEQAN
 cd ..
 mkdir build
 cd build
